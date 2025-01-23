e940273321c4c82817b48892605011ed6d42f877
hakixer <hakixer@gmail.com>
2025-01-23 12:09:28 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  PREFIX: process.env.PREFIX || "X-KINGx",
+  EMAIL: process.env.EMAIL || "hakiisdepressedgmail.com",
+  PASS: process.env.PASS || "mynameisexcel2",
+  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
+  IMAGE: process.env.IMAGE || "https://files.catbox.moe/y7memr.jpg",
+}
